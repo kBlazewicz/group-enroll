1f221e14c7143ddf1e3e2352ece49e7e8e2eba4c
KamilB <kblazewicz@student.agh.edu.pl>
2023-04-17 18:09:51 +0200

    added navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
+import ResponsiveAppBar from "./components/layout/Nav";
 import { StudentsForm } from "./components/students-form/StudentsForm";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/layout/Home";
 
 export const App = () => {
   return (
-    <StudentsForm></StudentsForm>
-    );
+    <div>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form creator" element={<StudentsForm />} />
+      </Routes>
+    </div>
+  );
 }
 
 
