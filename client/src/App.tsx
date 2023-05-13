9e6658d230c86ee9643bcc6270729bff5d25bac5
Kamil Blazewicz <92530490+kBlazewicz@users.noreply.github.com>
2023-05-13 16:06:18 +0200

    Tt 35 layout2 (#11)
    * added navbar
    
    * TT-35 added subpages
    
    * TT-35 fixed bug
    
    * TT-35 merge main to TT-35 conflicts
    
    * TT-35 conflicts fix
    
    ---------
    
    Co-authored-by: KamilB <kblazewicz@student.agh.edu.pl>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,51 @@
-import {Stack} from "@mui/material";
-import ShareFormCard from "./components/share-form/ShareFormCard";
+import ResponsiveAppBar from "./components/layout/Nav";
+import { StudentsForm } from "./components/students-form/StudentsForm";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/layout/Home";
+import FormCreator from "./components/layout/FormCreator";
+import FormAnswer from "./components/layout/FormAnswer";
+import Result from "./components/layout/Result";
+import { Stack } from "@mui/material";
+import { InputDateForm } from "./components/teacher-form/InputDateForm";
 import StudentDataForm from "./components/student-data-form/StudentDataForm";
-import {StudentsForm} from "./components/students-form/StudentsForm";
-import {InputDateForm} from "./components/teacher-form/InputDateForm";
+import ShareFormCard from "./components/share-form/ShareFormCard";
 
 export const App = () => {
-    return (
-        <Stack style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '40%',
-        }}>
+  return (
+    <Stack style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      height: '100%'
+    }}>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <Routes>
+        <Route path="/" element={
+          <Home>
+            {/* tutaj dodajemy kompoenenty dla strony wejściowej, nie wiem jeszcze co tu ma być */}
+            <div>Home page</div><ShareFormCard></ShareFormCard>
+          </Home>} />
+        <Route path="/form creator" element={
+          <FormCreator>
+            {/* tutaj dodajemy kompoenenty dla formularza prowadzącego */}
+            <div></div>
             <InputDateForm></InputDateForm>
-            <br/>
-            <StudentsForm></StudentsForm>
-            <br/>
             <ShareFormCard></ShareFormCard>
-            <br/>
+          </FormCreator>} />
+        <Route path="/form answers" element={
+          <FormAnswer>
+            {/* tutaj dodajemy kompoenenty dla formularza studenta */}
+            <div></div>
             <StudentDataForm></StudentDataForm>
-        </Stack>
-
-    );
+            <StudentsForm></StudentsForm>
+          </FormAnswer>} />
+        <Route path="/results" element={
+          <Result>
+            {/* tutaj dodajemy kompoenenty dla wynikow */}
+            <div></div><StudentsForm></StudentsForm>
+          </Result>} />
+      </Routes>
+    </Stack>
+  );
 }
 
